cce84dcd979c6beef717d638e2a89766154f3d7a
Ben <benranson5@gmail.com>
2021-03-28 11:45:05 +0100

    changed some prints to make my results more obvious

--- hero_story.py
+++ hero_story.py
@@ -7,21 +7,30 @@ story1 = {"start": "Once upon a time there was a sleepy boy",
 
 
 # Print the entire dictionary
-print(story1)
+print("\nPrint the entire dictionary:")
+for key, val in story1.items():
+    print(key + ":", val)
 
 # Print the type of your dictionary
+print("\nPrint the type of your dictionary:")
 print(type(story1))
 
 # Print only the keys
-print(story1.keys())
+print("\nPrint only the keys:")
+for key in story1.keys():
+    print(key)
 
 # Print only the values
-print(story1.values())
+print("\nPrint only the values:")
+for val in story1.values():
+    print(val)
 
 # Print the individual values using the keys (individually, lots of print commands)
+print("\nPrint the individual values using the keys:")
 print(story1["start"])
 print(story1["middle"])
 print(story1["end"])
 
 # Now let's add a new key:value pair
 story1["hero"] = "Arun"
+print("hero: " + story1["hero"])
